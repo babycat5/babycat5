3fcf9b9a690062e0058e8969939d208ebc7fd4be
babycat5 <pguo32616@gmail.com>
2021-07-21 13:09:47 +0800

    初始化

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { WelcomeComponent } from './components/home/welcome/welcome.component';
+import { SettingComponent } from './components/home/setting/setting.component';
+
+import { ProductComponent } from './components/product/product.component';
+import { PcateComponent } from './components/product/pcate/pcate.component';
+import { PlistComponent } from './components/product/plist/plist.component';
+
+import { NewsComponent } from './components/news/news.component';
+
+const routes: Routes = [
+  {
+
+    path: 'home', component: HomeComponent,
+
+    children: [
+
+      { path: 'welcome', component: WelcomeComponent },
+
+      { path: 'setting', component: SettingComponent },
+
+      { path: '**', redirectTo: 'welcome' }
+    ]
+
+
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+
+import { ViewportScroller } from '@angular/common';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-project';
+
+  @ViewChild('a') a: any;
+  @ViewChild('b') b: any;
+  @ViewChild('c') c: any;
+  @ViewChild('d') d: any;
+  @ViewChild('e') e: any;
+  @ViewChild('f') f: any;
+  constructor(
+    private el: ElementRef,
+    private scroller: ViewportScroller,
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewChecked(): void {
+    // console.log(this.myatt)
+
+  }
+
+  goPage() {
+    this.scroller.scrollToPosition([0, this.a.nativeElement.offsetTop]);
+  }
+
+  // addEventListener("load", function () {
+  //     setTimeout(hideURLbar, 0);
+  // }, false);
+
+  // function hideURLbar() {
+  //     window.scrollTo(0, 1);
+  // }
 }
